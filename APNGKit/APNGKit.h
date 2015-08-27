d97c933dfe34e9becc30847d3f586f3039d0a03f
onevcat <onevcat@gmail.com>
2015-08-27 11:57:53 +0900

    Build struct

--- APNGKit/APNGKit.h
+++ APNGKit/APNGKit.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char APNGKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <APNGKit/PublicHeader.h>
 
-
+#import "png.h"
