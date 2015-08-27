eff1eb4ca4321efc0f0073875421015e18b88c8f
onevcat <onevcat@gmail.com>
2015-08-27 10:48:34 +0900

    Init

---
+++ APNGKit/APNGKit.h
@@ -0,0 +1,19 @@
+//
+//  APNGKit.h
+//  APNGKit
+//
+//  Created by WANG WEI on 2015/08/27.
+//  Copyright © 2015年 OneV's Den. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APNGKit.
+FOUNDATION_EXPORT double APNGKitVersionNumber;
+
+//! Project version string for APNGKit.
+FOUNDATION_EXPORT const unsigned char APNGKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APNGKit/PublicHeader.h>
+
+
