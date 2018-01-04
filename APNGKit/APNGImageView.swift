9dae4f6b7859b315a80c6e296d95a079b06ff94c
HOJIN LEE <hojin@wisefour.com>
2018-01-04 18:52:26 +0900

    Fix bug animate fast when start again after stop
    For catching up a frame index value it animates too fast.

--- APNGKit/APNGImageView.swift
+++ APNGKit/APNGImageView.swift
@@ -232,6 +232,7 @@ open class APNGImageView: APNGView {
         repeated = 0
         lastTimestamp = 0
         currentPassedDuration = 0
+        currentFrameDuration = 0
         currentFrameIndex = 0
         
         timer = nil
