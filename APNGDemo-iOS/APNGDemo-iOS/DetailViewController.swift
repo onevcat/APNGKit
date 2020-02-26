4565cad30c46b8a20ed9d8dcf465b15223314f5a
onevcat <onevcat@gmail.com>
2020-02-26 23:52:26 +0900

    Upgrade project to Xcode 11.4

--- APNGDemo-iOS/APNGDemo-iOS/DetailViewController.swift
+++ APNGDemo-iOS/APNGDemo-iOS/DetailViewController.swift
@@ -60,6 +60,10 @@ class DetailViewController: UIViewController {
         }
     }
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
