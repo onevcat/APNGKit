7f61b2ee7416afe1ea79d7dac3fe3c0d848c50a0
onevcat <onevcat@gmail.com>
2021-10-31 17:13:39 +0900

    Add image attributes

--- Demo/Demo-macOS/SamplesViewController.swift
+++ Demo/Demo-macOS/SamplesViewController.swift
@@ -31,6 +31,12 @@ class SamplesViewController: NSViewController, NSTableViewDataSource, NSTableVie
     @IBOutlet weak var imageViewWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var imageSizeLabel: NSTextField!
+    @IBOutlet weak var imageDurationLabel: NSTextField!
+    @IBOutlet weak var imageFrameCountLabel: NSTextField!
+    
+    @IBOutlet weak var infoView: NSStackView!
+    @IBOutlet weak var previewLabel: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,6 +69,9 @@ class SamplesViewController: NSViewController, NSTableViewDataSource, NSTableVie
     
     func tableViewSelectionDidChange(_ notification: Notification) {
         guard tableView.selectedRow >= 0 else { return }
+        infoView.isHidden = false
+        previewLabel.isHidden = true
+        
         let name = Self.images[tableView.selectedRow]
         do {
             let image = try APNGImage(named: name)
@@ -70,7 +79,23 @@ class SamplesViewController: NSViewController, NSTableViewDataSource, NSTableVie
             imageViewWidthConstraint.constant = image.size.width
             imageViewHeightConstraint.constant = image.size.height
             
+            imageSizeLabel.stringValue = "\(image.size.width)x\(image.size.height) @ \(Int(image.scale))x"
+            imageDurationLabel.stringValue = "-"
+            imageFrameCountLabel.stringValue = "\(image.numberOfFrames)"
+            
+            image.onFramesInformationPrepared.delegate(on: self) { [weak image] (self, _) in
+                guard let image = image else { return }
+                switch image.duration {
+                case .loadedPartial:
+                    fatalError("All frames should be already loaded.")
+                case .full(let d):
+                    self.imageDurationLabel.stringValue = String(format: "%.3f", d) + " s"
+                }
+            }
+            
             imageView.image = image
+            
+            
         } catch {
             if let i = error.apngError?.normalImage {
                 imageView.staticImage = i
