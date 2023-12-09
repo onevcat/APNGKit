38f5e460be0d40c3f91f6623a2e0f191cee1eb3c
onevcat <onevcat@gmail.com>
2023-12-09 21:23:16 +0900

    Fix a runtime warning of nib

--- Demo/Demo-macOS/AppDelegate.swift
+++ Demo/Demo-macOS/AppDelegate.swift
@@ -10,9 +10,6 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
